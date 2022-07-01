30e823ce5a1bf3abd66713f9a290a320e0d4325a
SharkEzz <icraft640@gmail.com>
2022-07-01 21:48:32 +0200

    feat: added back api password check middleware

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -9,11 +9,29 @@ import (
 	"github.com/SharkEzz/provisiond/pkg/logging"
 )
 
+var apiPassword string
+
+func checkPassword(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("password") != apiPassword {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "invalid password")
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func StartAPI(host string, port uint16, password string) {
+	apiPassword = password
 	handlers := v1.API{}
 
-	http.HandleFunc("/v1/healthcheck", handlers.HandleGetHealthcheck)
-	http.HandleFunc("/v1/deploy", handlers.HandlePostDeploy)
+	healthcheckHandler := http.HandlerFunc(handlers.HandleGetHealthcheck)
+	deployHandler := http.HandlerFunc(handlers.HandlePostDeploy)
+
+	http.Handle("/v1/healthcheck", checkPassword(healthcheckHandler))
+	http.Handle("/v1/deploy", checkPassword(deployHandler))
 
 	logging.LogOut("Started API server")
 
